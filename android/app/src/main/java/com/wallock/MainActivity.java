86d5ab886c947b124f1a64ce1695dda0f4db9ea1
Quang Pham <qquang24t5@gmail.com>
2022-02-22 17:27:14 +0700

    Install React Navigation

--- android/app/src/main/java/com/wallock/MainActivity.java
+++ android/app/src/main/java/com/wallock/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wallock;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +12,10 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Wallock";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
