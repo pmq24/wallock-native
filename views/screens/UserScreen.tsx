c9b30e26d7a497a77c22bd5d6b3012085c7e7b42
Quang Pham <qquang24t5@gmail.com>
2022-02-23 11:02:13 +0700

    Add Categories screen

--- views/screens/UserScreen.tsx
+++ views/screens/UserScreen.tsx
@@ -1,10 +1,39 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { Button, SafeAreaView } from "react-native";
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from "@react-navigation/native-stack";
 
-export default function User() {
+import { CategoriesScreen } from ".";
+
+type RouteParamList = {
+  UserMainScreen: undefined;
+  CategoriesScreen: undefined;
+};
+
+const Stack = createNativeStackNavigator<RouteParamList>();
+
+export default function UserScreen() {
+  return (
+    <Stack.Navigator initialRouteName="UserMainScreen">
+      <Stack.Screen name="UserMainScreen" component={UserMainScreen} />
+      <Stack.Screen name="CategoriesScreen" component={CategoriesScreen} />
+    </Stack.Navigator>
+  );
+}
+
+type UserMainScreenProps = {
+  navigation: NativeStackNavigationProp<RouteParamList>;
+};
+
+function UserMainScreen({ navigation }: UserMainScreenProps) {
   return (
     <SafeAreaView>
-      <Text>User screen</Text>
+      <Button
+        title="Categories"
+        onPress={() => navigation.navigate("CategoriesScreen")}
+      />
     </SafeAreaView>
   );
 }
--- views/screens/index.ts
+++ views/screens/index.ts
@@ -1,1 +1,2 @@
+export { default as CategoriesScreen } from "./CategoriesScreen";
 export { default as UserScreen } from "./UserScreen";
