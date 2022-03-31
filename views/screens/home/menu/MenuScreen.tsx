c56a41ff5c8720e767a8ba5e262de3804127c40b
Quang Pham <qquang24t5@gmail.com>
2022-03-31 14:27:12 +0700

    `MenuScreen`: add Groups button
    Groups button will navigate user the `GroupsScreen` when pressed.

--- views/screens/home/menu/MenuScreen.tsx
+++ views/screens/home/menu/MenuScreen.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { Button, SafeAreaView, Text } from "react-native";
 
-export default function MenuScreen() {
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+
+import { MenuNavigatorParamList } from "../../NavigationTypes";
+
+type Props = NativeStackScreenProps<MenuNavigatorParamList, "MenuScreen">;
+
+export default function MenuScreen(props: Props) {
   return (
     <SafeAreaView>
       <Text>Menu</Text>
+      <Button title="Groups" onPress={navigateToGroupsScreen} />
     </SafeAreaView>
   );
+
+  function navigateToGroupsScreen() {
+    props.navigation.navigate("GroupsScreen");
+  }
 }
