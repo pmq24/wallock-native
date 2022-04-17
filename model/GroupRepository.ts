177e42cdfb79a0aa53df34d6cd8d6e93528cf349
Quang Pham <qquang24t5@gmail.com>
2022-04-17 20:55:26 +0700

    Change how `Vault` and `GroupRepository` can be created

--- model/GroupRepository.ts
+++ model/GroupRepository.ts
@@ -1,22 +1,66 @@
-import { Database, FindResponse } from "./Database";
+import { Database } from "./Database";
+import { GroupCreation } from "./groups/GroupCreation";
 import { GroupEntity } from "./groups/GroupEntity";
 
 export default class GroupRepository {
-  public constructor(private readonly database: Database) {}
+  public static async init(database: Database): Promise<GroupRepository> {
+    const groupRepo = new GroupRepository(database);
 
-  public async hasName(nameToCheck: string): Promise<boolean> {
-    const findResponse: FindResponse<GroupEntity> =
-      await new PouchDB<GroupEntity>(this.database.name).find({
-        selector: {
-          type: "GROUP",
-          name: nameToCheck,
-        },
+    const dbHasNotBeenCreatedBefore = await groupRepo.doesntHaveName("Root");
+
+    if (dbHasNotBeenCreatedBefore) {
+      await groupRepo.database.put<GroupEntity>({
+        type: "GROUP",
+        _id: "Root",
+        parentId: undefined,
       });
 
-    return findResponse.docs.length === 1;
+      await groupRepo.database.put<GroupEntity>({
+        type: "GROUP",
+        _id: "Income",
+        parentId: "Root",
+      });
+
+      await groupRepo.database.put<GroupEntity>({
+        type: "GROUP",
+        _id: "Expense",
+        parentId: "Root",
+      });
+    }
+
+    return groupRepo;
+  }
+
+  public async create(groupCreation: GroupCreation): Promise<GroupEntity> {
+    if (await this.hasName(groupCreation.name))
+      throw new Error(`Group name "${groupCreation.name}" already exists`);
+
+    if (await this.doesntHaveName(groupCreation.parentName))
+      throw new Error(
+        `Parent group "${groupCreation.parentName}" doesn't exist`
+      );
+
+    await this.database.put<GroupEntity>({
+      type: "GROUP",
+      _id: groupCreation.name,
+      parentId: groupCreation.parentName,
+    });
+
+    return await this.database.get<GroupEntity>(groupCreation.name);
+  }
+
+  public async hasName(nameToCheck: string): Promise<boolean> {
+    try {
+      await this.database.get<GroupEntity>(nameToCheck);
+    } catch (e) {
+      return false;
+    }
+    return true;
   }
 
   public async doesntHaveName(nameToCheck: string): Promise<boolean> {
-    return !this.hasName(nameToCheck);
+    return !(await this.hasName(nameToCheck));
   }
+
+  private constructor(private readonly database: Database) {}
 }
--- model/Vault.ts
+++ model/Vault.ts
@@ -2,12 +2,27 @@ import { connectToDatabase, Database } from "./Database";
 import GroupRepository from "./GroupRepository";
 
 export default class Vault {
-  public constructor(readonly name: string) {
-    this.database = connectToDatabase(name);
-    this.groups = new GroupRepository(this.database);
+  public static async init(name: string): Promise<Vault> {
+    const vault = new Vault(name);
+
+    vault._groups = await GroupRepository.init(vault.database);
+
+    return vault;
   }
 
-  public readonly groups: GroupRepository;
+  public async destroy() {
+    this.database.destroy();
+  }
+
+  public get groups(): GroupRepository {
+    return this._groups!;
+  }
+
+  private constructor(readonly name: string) {
+    this.database = connectToDatabase(name);
+  }
+
+  private _groups: GroupRepository | undefined;
 
   private readonly database: Database;
 }
