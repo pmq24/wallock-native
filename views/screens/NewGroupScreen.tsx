31c356e46c009ce48e5da3e95d9183ffd3553122
Quang Pham <quangpham.245@protonmail.com>
2022-04-25 15:42:30 +0700

    Add `LoadingIndicator` in `NewGroupScreen`

--- views/screens/NewGroupScreen.tsx
+++ views/screens/NewGroupScreen.tsx
@@ -7,6 +7,7 @@ import { GroupPicker, TextField } from "../components";
 import { RootNavigatorScreenProps } from "./NavigationTypes";
 import Vault from "../../model/Vault";
 import VaultContext from "../../model/VaultContext";
+import LoadingIndicator from "../components/LoadingIndicator";
 
 type Props = RootNavigatorScreenProps<"NewGroupScreen">;
 
@@ -29,50 +30,55 @@ export default function NewGroupScreen(props: Props) {
   });
 
   return (
-    <Formik<FormFieldsTypes>
-      innerRef={formikRef}
-      initialValues={{
-        groupName: "",
-        parentGroupName: "",
-      }}
-      validationSchema={Yup.object().shape({
-        groupName: Yup.string()
-          .required("Required")
-          .min(1, "Required")
-          .test(
-            "isUnique",
-            `Group with that name already exists`,
-            async (value, _) => {
-              if (!vault) return true;
-              return await vault.groups.doesntHaveName(value!);
-            }
-          ),
-        parentGroupName: Yup.string().required("Required"),
-      })}
-      onSubmit={async (values) => {
-        vault?.groups.create({
-          name: values.groupName,
-          parentName: values.parentGroupName,
-        });
-      }}
-    >
-      {(formik) => (
-        <SafeAreaView style={{ padding: 24 }}>
-          <TextField
-            label="Group name"
-            onChangeText={formik.handleChange("groupName")}
-            onBlur={formik.handleBlur("groupName")}
-            value={formik.values.groupName}
-            errorMessage={formik.errors.groupName}
-          />
-          <GroupPicker
-            label="Parent group"
-            onPicked={formik.handleChange("parentGroupName")}
-            onBlur={formik.handleBlur("parentGroupName")}
-            value={formik.values.parentGroupName}
-          />
-        </SafeAreaView>
+    <>
+      {vault === undefined ? (
+        <LoadingIndicator />
+      ) : (
+        <Formik<FormFieldsTypes>
+          innerRef={formikRef}
+          initialValues={{
+            groupName: "",
+            parentGroupName: "",
+          }}
+          validationSchema={Yup.object().shape({
+            groupName: Yup.string()
+              .required("Required")
+              .min(1, "Required")
+              .test(
+                "isUnique",
+                `Group with that name already exists`,
+                async (value, _) => {
+                  return await vault.groups.doesntHaveName(value!);
+                }
+              ),
+            parentGroupName: Yup.string().required("Required"),
+          })}
+          onSubmit={async (values) => {
+            vault.groups.create({
+              name: values.groupName,
+              parentName: values.parentGroupName,
+            });
+          }}
+        >
+          {(formik) => (
+            <SafeAreaView style={{ padding: 24 }}>
+              <TextField
+                label="Group name"
+                onChangeText={formik.handleChange("groupName")}
+                onBlur={formik.handleBlur("groupName")}
+                value={formik.values.groupName}
+                errorMessage={formik.errors.groupName}
+              />
+              <GroupPicker
+                label="Parent group"
+                onPicked={formik.handleChange("parentGroupName")}
+                onBlur={formik.handleBlur("parentGroupName")}
+                value={formik.values.parentGroupName}
+              />
+            </SafeAreaView>
+          )}
+        </Formik>
       )}
-    </Formik>
+    </>
   );
 }
